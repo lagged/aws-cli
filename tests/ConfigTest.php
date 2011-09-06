a7d96e400e76dc596525cadd59408969e490395b
till <till@php.net>
2011-09-06 15:11:37 -0400

    cover exception

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,15 +1,41 @@
 <?php
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Lagged\AWS\Config $config
+     */
+    protected $config;
+
+    /**
+     * setUp
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $file         = dirname(__DIR__) . '/etc/config.ini';
+        $this->config = new Lagged\AWS\Config($file);
+    }
+
+    /**
+     * Basic test to see if our values end up in expected objects.
+     *
+     * @return void
+     */
     public function testThis()
     {
-        $file = dirname(__DIR__) . '/etc/config.ini';
-        $config = new Lagged\AWS\Config($file);
-
-        $aws = $config->aws;
+        $aws = $this->config->aws;
 
         $this->assertInstanceOf('stdClass', $aws);
         $this->assertObjectHasAttribute('key', $aws);
         $this->assertObjectHasAttribute('secret', $aws);
+    }
+
+    /**
+     * @expectedException \RangeException
+     */
+    public function testException()
+    {
+        $this->config->cloud;
     }
 }
