8074ec2ad649a5c0e56a4e7e86e0a9ce48dc1750
till <till@php.net>
2011-09-06 19:38:09 -0400

    command/task to show all available

---
+++ src/Cli/Command/Help.php
@@ -0,0 +1,20 @@
+<?php
+namespace Lagged\AWS\Cli\Command;
+
+use Lagged\AWS\Cli\Command as Command;
+
+class Help extends Command
+{
+    public function __construct(\Lagged\AWS\Config $config = null)
+    {
+    }
+
+    public function Help()
+    {
+        $data = array();
+        foreach (glob(__DIR__ . '/*.php') as $file) {
+            $data[] = '--' . strtolower(substr(basename($file), 0, -4));
+        }
+        return $data;
+    }
+}
