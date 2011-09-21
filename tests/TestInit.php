f825b3e7e95c6f2776513d973ef38d130c325018
till <till@php.net>
2011-09-21 14:13:17 +0200

    update path in testhelper

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'PHPUnit/Autoload.php';
 
-require_once dirname(__DIR__) . '/src/Autoload.php';
+require_once dirname(__DIR__) . '/src/Lagged/AWS/Autoload.php';
 spl_autoload_register(array('Lagged\AWS\Autoload', 'load'));
